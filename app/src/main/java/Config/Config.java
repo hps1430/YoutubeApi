e9035b40196e5929b2c06ad72168db7bac4212e3
harsh singh <hpsindia1430@gmail.com>
2017-08-03 14:34:21 +0530

    Initial commit

---
+++ app/src/main/java/Config/Config.java
@@ -0,0 +1,14 @@
+package Config;
+
+/**
+ * Created by harsh singh on 27-07-2017.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAvNJt1y10kEeJfmi2qcqwEFz6mCaT_Khk";
+
+}
